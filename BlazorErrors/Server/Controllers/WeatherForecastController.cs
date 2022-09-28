2638b76fbcf53ffa28d31975f3f5fbe0891b64a9
Cyril Gupta <5773498+cyrilgupta@users.noreply.github.com>
2022-09-28 21:27:52 +0400

    init

---
+++ BlazorErrors/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using BlazorErrors.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorErrors.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            try
+            {
+                throw new Exception("Something wrong");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Something happened");
+                return null;
+            }
+        }
+    }
+}
